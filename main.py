3bb1970549639e3ac72847e89c431fe059d48349
parkgraee <hunolstar@gmail.com>
2026-07-12 20:19:48 +0900

    Update main.py

--- main.py
+++ main.py
@@ -9,7 +9,7 @@ app = FastAPI()
 # Google API 키 설정
 genai.configure(api_key=os.environ.get("GOOGLE_API_KEY"))
 
-model = genai.GenerativeModel('gemini-1.5-flash-8b')
+model = genai.GenerativeModel("gemini-2.5-flash")
 
 
 class TextRequest(BaseModel):
